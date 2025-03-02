f79d6a1c339ec39fc7d45325aa24e57a870d07f5
OGrecky <39439179+OGrecky@users.noreply.github.com>
2025-03-02 11:25:38 +0300

    Updated predictDirection

--- lib/predict.cpp
+++ lib/predict.cpp
@@ -1,3 +1,4 @@
+#include "lpsolver/matrices.hpp"
 #ifdef SUPER
 #include <Eigen/SuperLUSupport>
 #endif
@@ -5,21 +6,101 @@
 
 namespace LPSolver {
     Delta predictDirection(const Problem &prob, const Position &position) {
-        Matrix invH = position.constructInvH();
-        Matrix AT = prob.A.transpose();
-        #ifdef SUPER
-        Eigen::SuperLU<Eigen::SparseMatrix<double>> slu;
-        #else
-        Eigen::SparseLU<Eigen::SparseMatrix<double>> slu;
-        #endif
-        slu.compute(prob.A * invH * AT);
-        assert(slu.info() == Eigen::Success);
+        if (position.cnt_free_indices() != 0) {
+            Matrix A1 = select_columns(prob.A, position.get_free_indices());
+            Matrix A2 = select_columns(prob.A, position.get_remaining_indices());
 
-        Vector dy = slu.solve(prob.b);
-        Vector ds = -AT * dy;
-        Vector dx = -invH * ds - position.x;
+            Vector x2 = position.get_x_remaining();
+            Vector s = position.get_s_remaining();
+            Matrix invH = position.constructInvH();
 
-        return Delta(position.n, position.m, dx, dy, ds);
+            Matrix N = A2 * invH * A2.transpose();
+            Matrix M = construct_block({{A1, N}, {Matrix(A1.transpose().rows(), A1.cols()), A1.transpose()}});
+
+            #ifdef SUPER
+            Eigen::SuperLU<Eigen::SparseMatrix<double>> slu;
+            #else
+            Eigen::SparseLU<Eigen::SparseMatrix<double>> slu;
+            #endif
+
+            slu.compute(M);
+            assert(slu.info() == Eigen::Success);
+
+            Vector res1 = A2 * x2;
+            Vector right_v(res1.rows() + position.cnt_free_indices());
+            for (int i = 0; i < res1.rows(); ++i) {
+                right_v(i) = res1(i);
+            }
+            for (int i = res1.rows(); i < right_v.rows(); ++i) {
+                right_v(i) = 0;
+            }
+
+            Vector sol = slu.solve(right_v);
+
+            Vector delta_x1(position.cnt_free_indices());
+            Vector delta_y(sol.rows() - position.cnt_free_indices());
+            for (int i = 0; i < position.cnt_free_indices(); ++i) {
+                delta_x1(i) = sol(i);
+            }
+            for (int i = position.cnt_free_indices(); i < sol.rows(); ++i) {
+                delta_y(i - position.cnt_free_indices()) = sol(i);
+            }
+
+            Vector delta_s = -A2.transpose() * delta_y;
+
+            Vector delta_x2 = -invH * delta_s - x2;
+
+            Vector true_deltax(position.n);
+            true_deltax.setZero();
+            std::vector<int> free_indices = position.get_free_indices();
+            std::vector<int> remaining_indices = position.get_remaining_indices();
+            for (size_t i = 0; i < free_indices.size(); ++i) {
+                true_deltax(free_indices[i]) = delta_x1(i);
+            }
+            for (size_t i = 0; i < remaining_indices.size(); ++i) {
+                true_deltax(remaining_indices[i]) = delta_x2(i);
+            }
+
+            Vector true_deltas(position.n);
+            true_deltas.setZero();
+            for (size_t i = 0; i < remaining_indices.size(); ++i) {
+                true_deltas(remaining_indices[i]) = delta_s(i);
+            }
+
+            return Delta(position.n, position.m, true_deltax, delta_y, true_deltas);
+        } else {
+            Matrix A2 = select_columns(prob.A, position.get_remaining_indices());
+            Vector x2 = position.get_x_remaining();
+            Vector s = position.get_s_remaining();
+            Matrix invH = position.constructInvH();
+
+            #ifdef SUPER
+            Eigen::SuperLU<Eigen::SparseMatrix<double>> slu;
+            #else
+            Eigen::SparseLU<Eigen::SparseMatrix<double>> slu;
+            #endif
+
+            Matrix slu_matr = A2 * invH * A2.transpose();
+            slu.compute(slu_matr);
+            assert(slu.info() == Eigen::Success);
+
+            Vector delta_y = slu.solve(A2 * x2);
+            Vector delta_s = -A2.transpose() * delta_y;
+            Vector delta_x2 = -invH * delta_s - x2;
+
+            Vector true_deltax(position.n);
+            true_deltax.setZero();
+            Vector true_deltas(position.n);
+            true_deltas.setZero();
+
+            std::vector<int> remaining_indices = position.get_remaining_indices();
+            for (size_t i = 0; i < remaining_indices.size(); ++i) {
+                true_deltax(remaining_indices[i]) = delta_x2(i);
+                true_deltas(remaining_indices[i]) = delta_s(i);
+            }
+
+            return Delta(position.n, position.m, true_deltax, delta_y, true_deltas);
+        }
     }
 
     double predictLength(const Position &position, const Delta &delta, double gamma_predict) {
